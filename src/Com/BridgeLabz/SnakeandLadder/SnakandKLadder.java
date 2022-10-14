570d40565e14dcca6908b791494500ed43226ff3
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-14 08:35:50 +0530

    Roll_A_Die

--- src/Com/BridgeLabz/SnakeandLadder/SnakandKLadder.java
+++ src/Com/BridgeLabz/SnakeandLadder/SnakandKLadder.java
@@ -1,12 +1,18 @@
 package Com.BridgeLabz.SnakeandLadder;
+
+import java.util.Random;
+
 public class SnakandKLadder{
 	static final int STARTING_POSITION = 0;
 	static final int ENDING_POSITION = 100;
 	public static void main(String[] args) {
 		int startingPosition = 0;
-        
+
 		System.out.println("Welcome to the Snake and ladder program ");
 		System.out.println("Starting Position of the Single Player is : "+startingPosition);
+		Random ran = new Random();
+		int dice = ran.nextInt(6)+1;
+		System.out.println("dice: "+dice);
 	}
 }
 
