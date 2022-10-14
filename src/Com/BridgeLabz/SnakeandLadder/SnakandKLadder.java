6a7c864116ecf015beb0e7e4434e3642fc086cc0
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-14 08:12:18 +0530

    Welcome

--- src/Com/BridgeLabz/SnakeandLadder/SnakandKLadder.java
+++ src/Com/BridgeLabz/SnakeandLadder/SnakandKLadder.java
@@ -1,11 +1,2 @@
-package Com.BridgeLabz.SnakeandLadder;
 
-public class SnakandKLadder {
-	static final int STARTING_POSITION = 0;
-	static final int ENDING_POSITION = 100;
-	public static void main(String[] args) {
-		 int startingPosition = 0;
-         System.out.println("Starting Position of the Single Player is : "+startingPosition);
-	}
 
-}
