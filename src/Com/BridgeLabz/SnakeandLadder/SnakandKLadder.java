d814100931df0e20c62033dcd26f2efb2bf7c360
Namdev Chavan <109471216+namdevchavanmnb@users.noreply.github.com>
2022-10-15 16:26:36 +0530

    Update SnakandKLadder.java

--- src/Com/BridgeLabz/SnakeandLadder/SnakandKLadder.java
+++ src/Com/BridgeLabz/SnakeandLadder/SnakandKLadder.java
@@ -1,7 +1,6 @@
 package Com.BridgeLabz.SnakeandLadder;
 public class SnakandKLadder{
 	static final int STARTING_POSITION = 0;
-	static final int ENDING_POSITION = 100;
 	public static void main(String[] args) {
 		int startingPosition = 0;
         
